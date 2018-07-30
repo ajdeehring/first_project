42ec16d0da30cc3e2a502ecafe9d68a233156578
Ajdee <ajdeehring@gmail.com>
2018-07-30 15:46:49 -0700

    Adam commmiteed this

--- myfirstproject.py
+++ myfirstproject.py
@@ -1,6 +1,26 @@
 def main():
-    print("hello world")
+    name = input("What is your name?")
+    print("Hello ", name)
 
+    x = 10
+    print(x)
+
+    number = int(input("Enter in a number: "))
+    if number > 10:
+        print(number, "is greater than 10")
+    elif number == 10:
+        print("that number is 10")
+    else:
+        print(number, "is less than 10")
+    number = int(input("Enter in a number: "))
+    if number % 2 == 0:
+        print(number, "is even")
+    else:
+        print(number, " is odd")
+
+    my_list = ["Adam","Dylan","Kira","Lake","Mizuki","Nathaniel","Roman","Sheridan","Wyatt"]
+    for name in my_list:
+        print(name)
 
 if __name__ == "__main__":
     main()
